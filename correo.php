e9cdd4428e25c4b94e03c2057ce76aaf3638b15d
Colpilar <78564487+Colpilar@users.noreply.github.com>
2021-02-07 11:15:54 -0500

    Update correo.php

--- correo.php
+++ correo.php
@@ -1,6 +1,6 @@
 <?php
 
-$destinatario = 'gracesilvaduarte@outlook.es';
+$destinatario = "gracesilvaduarte@outlook.es";
 
 $nombre = $_POST['nombre'];
 $tipo_tramite = $_POST['tipo_tramite'];
@@ -12,7 +12,7 @@ $header = "Se ha recibido un mensaje desde la Página Colpilar Cúcuta";
 
 $mensajecompleto = $comments." \n Atentamente: $nombre. Teléfono: $phone";
 
-mail($destinatario, $tipo_tramite, $mensajecompleto, $header);
+$send = mail($destinatario, $tipo_tramite, $mensajecompleto, $header);
 
 echo "<script>alert('Mensaje Enviado Exitosamente')</script>";
 echo "<script>setTimeout(\"location.href='contact.html'\", 1000)</script>";
